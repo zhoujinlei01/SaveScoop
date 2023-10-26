885d1c4517776ca1340b4da396b6b98f7da6e870
周金磊 <978100606@qq.com>
2023-10-26 15:41:01 +0800

    feat:新增 common 库，将 BaseActivity 和 ActivityUtil 类迁移到 common 库中。

---
+++ common/src/main/java/com/lay/common/base/BaseActivity.java
@@ -0,0 +1,141 @@
+package com.lay.common.base;
+
+import android.content.Context;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Looper;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.lay.common.util.ActivityUtil;
+
+
+/**
+ * @author sm3243
+ */
+public abstract class BaseActivity extends AppCompatActivity {
+    private static Toast toast;
+    public Context context;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        ActivityUtil.getInstance().addActivity(this);
+        setContentView(getLayoutId());
+        context = this;
+        initView();
+        initData();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        ActivityUtil.getInstance().removeActivity(this);
+    }
+
+    /**
+     * 界面初始化操作
+     */
+    protected abstract void initView();
+
+    /**
+     * 数据初始化操作
+     */
+    protected abstract void initData();
+
+    /**
+     * 获取布局ID
+     *
+     * @return 布局ID
+     */
+    protected abstract int getLayoutId();
+
+    /**
+     * 保证按钮一秒只会响应一次
+     */
+    public abstract class OnSingleClickListener implements View.OnClickListener {
+        private static final int MIN_CLICK_DELAY_TIME = 1000;
+        private long lastClickTime = 0;
+
+        public abstract void onSingleClick(View view);
+
+        @Override
+        public void onClick(View view) {
+            long curClickTime = System.currentTimeMillis();
+            if ((curClickTime - lastClickTime) >= MIN_CLICK_DELAY_TIME) {
+                lastClickTime = curClickTime;
+                onSingleClick(view);
+            }
+        }
+    }
+
+    /**
+     * 同一按钮短时间可以重复响应点击事件
+     */
+    public abstract class onMultiClickListener implements View.OnClickListener {
+        public abstract void onMultiClick(View view);
+
+        @Override
+        public void onClick(View view) {
+            onMultiClick(view);
+        }
+    }
+
+    /**
+     * 显示提示信息
+     *
+     * @param msg
+     */
+    public void showToast(String msg) {
+        try {
+            if (toast == null) {
+                toast = Toast.makeText(context, msg, Toast.LENGTH_SHORT);
+            } else {
+                toast.setText(msg);
+            }
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    toast.show();
+                }
+            });
+        } catch (Exception e) {
+            e.printStackTrace();
+            Looper.prepare();
+            Toast.makeText(context, msg, Toast.LENGTH_SHORT).show();
+            Looper.loop();
+        }
+    }
+
+    /**
+     * 隐藏状态栏
+     */
+    protected void hideStatusBar() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        }
+    }
+
+    /**
+     * 隐藏标题栏
+     */
+    protected void hideActionBar() {
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().hide();
+        }
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if(keyCode == KeyEvent.KEYCODE_BACK){
+            this.finish();
+            ActivityUtil.getInstance().removeActivity(this);
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+}
